9904a0579b8e88ff525aa038866f5b5aee6969b6
chiennx <noreply.saobacdau@gmail.com>
2023-06-13 23:15:30 +0700

    Initial commit

---
+++ src/test/java/org/smart/asia/ecommerce/EcommerceApplicationTests.java
@@ -0,0 +1,13 @@
+package org.smart.asia.ecommerce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcommerceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
